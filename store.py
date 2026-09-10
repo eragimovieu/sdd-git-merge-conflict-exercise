8d875a1d3720bbacdf600bb8f67b0c8fcbe67288
Elshan Ragimov <elshan@MacBook-Air-Elshan.local>
2026-09-10 11:28:59 +0200

    Add shipping

--- store.py
+++ store.py
@@ -12,7 +12,8 @@ def calculate_total(
 
     # TODO: apply your assigned feature's pricing rule here, gated behind
     # its argument (apply_discount, apply_tax, or apply_shipping)
-
+    if apply_shipping:
+        total += 5.0
     return total
 
 
